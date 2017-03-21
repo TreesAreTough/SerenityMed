e8148b757419dd5b84e84e3f52c26c28c6b0bf09
awhite9 <ajumper12@gmail.com>
2017-03-20 20:32:55 -0500

    Dashboard completely hooked up

---
+++ app/models/LabManager.java
@@ -0,0 +1,32 @@
+package models;
+import javax.persistence.*;
+import java.sql.Date;
+@Entity
+public class LabManager
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="LAB_PULLED_ID")
+    public Long labPulledId;
+
+    @Column (name ="DATE_TAKEN")
+    public Date dateTaken;
+
+    @Column (name ="PATIENT_ID")
+    public Long patientId;
+
+    @Column (name ="LAB_ID")
+    public Long labId;
+
+    @Column (name ="DOCTOR_ID")
+    public Long doctorId;
+
+    @Column (name="VALUE")
+    public String value;
+
+    @Column(name="LAB_NAME")
+    public String labName;
+
+    @Column(name="DOC_NAME")
+    public String docName;
+}
---
+++ app/models/LineChart.java
@@ -0,0 +1,20 @@
+package models;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Entity
+public class LineChart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "DATE_TAKEN")
+    public LocalDate dateTaken;
+
+    @Column(name = "val1")
+    public String val1;
+
+    @Column(name = "val2")
+    public String val2;
+}
