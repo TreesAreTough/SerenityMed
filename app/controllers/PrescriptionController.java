437bd2686b30b39baa2a4f1a2fd55b254d1a5eed
awhite9 <ajumper12@gmail.com>
2017-03-26 22:35:37 -0500

    took out extra pages

--- app/controllers/PrescriptionController.java
+++ app/controllers/PrescriptionController.java
@@ -252,5 +252,20 @@ public class PrescriptionController  extends Controller
         return redirect(routes.PrescriptionController.getPrescriptionManager());
     }
 
+    @Transactional(readOnly = true)
+    public Result getDrugInteraction()
+    {
+        List<Interaction> interaction = (List<Interaction>) jpaApi.em().createNativeQuery("select di.drug_interaction_id, m.MEDICATION_NAME as \"drug1Name\", di.rxcui, di.inter_rxcui, md.medication_name as \"drug2Name\", di.INTER_DESCRIPTION from drug_interaction di\n" +
+                "join medication m on m.RXCUI = di.RXCUI \n" +
+                "join medication md on md.RXCUI = di.INTER_RXCUI\n" +
+                "where di.rxcui in (select rxcui from prescription p  join medication m on p.MEDICATION_ID = m.MEDICATION_ID where patient_id = 11)\n" +
+                "and di.inter_rxcui in (select rxcui from prescription p  join medication m on p.MEDICATION_ID = m.MEDICATION_ID where patient_id = 11)\n" +
+                "and m.MEDICATION_NAME in (select medication_name from prescription p join medication m on p.MEDICATION_ID = m.MEDICATION_ID where patient_id = 11)\n" +
+                "and md.MEDICATION_NAME in (select medication_name from drug_interaction dri join medication md on dri.INTER_RXCUI = md.RXCUI)", Interaction.class).getResultList();
+
+        return ok(views.html.drugInteractionPage.render(interaction));
+
+    }
+
 
 }
